a8b3924e35582481037eb15c3c026063b3ba2c3e
Pedro Camacho <pedro.camacho@gmail.com>
2020-01-26 10:29:10 +0100

    142 MiYazJE

--- src/DavidCachero/Main142.java
+++ src/DavidCachero/Main142.java
@@ -5,28 +5,27 @@ import java.util.Scanner;
 
 public class Main142 {
 	public static void main(String[] args) {
-		int chico;
-		int salvado;
-		int eliminando;
-		boolean bucle = true;
-		Scanner in = new Scanner(System.in);
-		while (bucle) {
-			chico = in.nextInt();
-			salvado = in.nextInt();
-			if (chico == 0 && salvado == 0)
-				bucle = false;
-			else {
-				ArrayList<Integer> partida = new ArrayList<Integer>();
-				for (int i = 1; i <= chico; i++)
-					partida.add(i);
-				eliminando = 0;
-				while (partida.size() > 1) {
-					eliminando = (eliminando + salvado);
-					eliminando = (eliminando % partida.size());
-					partida.remove(eliminando);
-				}
-				System.out.println(partida.get(0));
+		int jugadores;
+		int saltos;
+		int eliminar;
+		
+		Scanner s = new Scanner(System.in);
+		ArrayList<Integer> juego;
+		while (true) {
+			jugadores = s.nextInt();
+			saltos = s.nextInt();
+			if (jugadores == 0 && saltos == 0)
+				break;
+			juego = new ArrayList<>();
+			for (int i = 1; i <= jugadores; i++)
+				juego.add(i);
+			eliminar = 0;
+			while (juego.size() > 1) {
+				eliminar += saltos;
+				eliminar %= juego.size();
+				juego.remove(eliminar);
 			}
+			System.out.println(juego.get(0));
 		}
 	}
 }
