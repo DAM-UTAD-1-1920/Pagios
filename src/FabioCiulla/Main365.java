91b0e261b61bdda5ac072ee576f24a1b4e45b1ba
Pedro Camacho <pedro.camacho@gmail.com>
2020-01-27 23:05:12 +0100

    365 Fabio

---
+++ src/FabioCiulla/Main365.java
@@ -0,0 +1,37 @@
+package FabioCiulla;
+
+import java.util.Scanner;
+
+public class Main365 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int casos = sc.nextInt();
+		int ninos;
+		boolean aitor;
+		int tiempo;
+		int posAitor;
+		int[] cola;
+		for (int x = 0; x < casos; x++) {
+			ninos = sc.nextInt();
+			posAitor = sc.nextInt();
+			cola = new int[ninos];
+			tiempo = 0;
+			aitor = false;
+			for (int i = 0; i < ninos; i++)
+				cola[i] = sc.nextInt();
+			while (aitor == false) {
+				for (int i = 0; i < cola.length; i++) {
+					if (cola[i] != 0) {
+						tiempo += 2;
+						cola[i]--;
+					}
+					if (cola[posAitor - 1] == 0) {
+						aitor = true;
+						break;
+					}
+				}
+			}
+			System.out.println(tiempo);
+		}
+	}
+}
