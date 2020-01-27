fc41252bd1f722dadfaba0e0108ab7cafdc502a4
Pedro Camacho <pedro.camacho@gmail.com>
2020-01-27 23:03:01 +0100

    139 MiYazJE

--- src/MiguelAngelGarre/Main139.java
+++ src/MiguelAngelGarre/Main139.java
@@ -1,14 +1,19 @@
 package MiguelAngelGarre;
 
-import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Main139 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int num, digito;
+		int suma;
+		String temp;
+		Set<Integer> set;
 		while (true) {
-			int num = sc.nextInt();
-			ArrayList<Integer> set = new ArrayList<Integer>(1);
+			num = sc.nextInt();
+			set = new TreeSet<>();
 			if (num == 0)
 				break;
 			set.add(num);
@@ -17,23 +22,22 @@ public class Main139 {
 			else {
 				System.out.print(num + " - ");
 				while (true) {
-					int digitos;
-					int total = 0;
-					String aux = String.valueOf(num);
-					for (int i = aux.length() - 1; i >= 0; i--) {
-						digitos = Integer.parseInt("" + aux.charAt(i));
-						total += digitos * digitos * digitos;
+					suma = 0;
+					temp = String.valueOf(num);
+					for (int i = temp.length() - 1; i >= 0; i--) {
+						digito = Integer.parseInt("" + temp.charAt(i));
+						suma += digito * digito * digito;
 					}
-					if (total == 1) {
-						System.out.println(total + " -> cubifinito.");
+					if (suma == 1) {
+						System.out.println(suma + " -> cubifinito.");
 						break;
-					} else if (set.contains(total)) {
-						System.out.println(total + " -> no cubifinito.");
+					} else if (set.contains(suma)) {
+						System.out.println(suma + " -> no cubifinito.");
 						break;
 					} else {
-						set.add(total);
-						num = total;
-						System.out.print(total + " - ");
+						set.add(suma);
+						num = suma;
+						System.out.print(suma + " - ");
 					}
 				}
 			}
