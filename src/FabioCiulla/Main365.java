0b07b6d958f886eb8c51622835135425d013028d
Pedro Camacho <pedro.camacho@gmail.com>
2020-01-27 23:07:53 +0100

    139 MiYazJE

--- src/FabioCiulla/Main365.java
+++ src/FabioCiulla/Main365.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 
 public class Main365 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int casos = sc.nextInt();
+		Scanner s = new Scanner(System.in);
+		int casos = s.nextInt();
 		int ninos;
 		boolean aitor;
 		int tiempo;
 		int posAitor;
 		int[] cola;
 		for (int x = 0; x < casos; x++) {
-			ninos = sc.nextInt();
-			posAitor = sc.nextInt();
+			ninos = s.nextInt();
+			posAitor = s.nextInt();
 			cola = new int[ninos];
 			tiempo = 0;
 			aitor = false;
 			for (int i = 0; i < ninos; i++)
-				cola[i] = sc.nextInt();
+				cola[i] = s.nextInt();
 			while (aitor == false) {
 				for (int i = 0; i < cola.length; i++) {
 					if (cola[i] != 0) {
