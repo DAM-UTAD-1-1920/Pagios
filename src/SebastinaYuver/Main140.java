13d894f79ad9b1062cb870c14bd9686b7170c8c7
Pedro Camacho <pedro.camacho@gmail.com>
2020-01-27 22:48:03 +0100

    140 MiYazJE

--- src/SebastinaYuver/Main140.java
+++ src/SebastinaYuver/Main140.java
@@ -5,27 +5,27 @@ import java.util.Scanner;
 public class Main140 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String num;
+		String total;
 		int suma;
-		int unidad;
-		num = sc.nextLine();
-		if (!("" + num.charAt(0)).equals("-")) {
-			do {
-				suma = 0;
-				for (int i = 0; i < num.length(); i++) {
-					unidad = Integer.parseInt("" + num.charAt(i));
-					suma = suma + unidad;
+		int digito;
+		do {
+			total = sc.nextLine();
+			suma = 0;
+			if (("" + total.charAt(0)).equals("-")) {
+				break;
+			}
+			for (int i = 0; i < total.length(); i++) {
+				digito = Integer.parseInt("" + total.charAt(i));
+				suma += digito;
 
-					if (i == num.length() - 1) {
-						System.out.print(unidad + " = " + suma);
-					} else {
-						System.out.print(unidad + " + ");
-					}
+				if (i == total.length() - 1) {
+					System.out.print(digito + " = " + suma);
+				} else {
+					System.out.print(digito + " + ");
 				}
-				System.out.println();
-				num = sc.nextLine();
-			} while ((0 <= Integer.parseInt(num)) && !("" + num.charAt(0)).equals("-"));
-		}
-		sc.close();
+			}
+			System.out.println();
+		} while (0 <= Integer.parseInt(total));
 	}
+
 }
